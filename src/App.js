3f379031851935947838c8f30c970b21d484a79e
MOMGrom <gromachhe.mom@yandex.ru>
2024-07-03 01:47:56 +0300

    graph2

--- src/App.js
+++ src/App.js
@@ -37,8 +37,9 @@ function App() {
 
   return (
     <div className="App">
-      <CodeInput AnlzBtnClick={AnlzBtnClick}/>
-      {graph ? <Graph Graph={graph}/> : <></>}
+      <Graph/>
+      {/* <CodeInput AnlzBtnClick={AnlzBtnClick}/>
+      {graph ? <Graph Graph={graph}/> : <></>} */}
       {/* 
       <Graph/>
       {matrix ? <Matrix Matrix={matrix}/> : <></>} */}
