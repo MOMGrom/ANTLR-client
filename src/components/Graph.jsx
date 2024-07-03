9213464a1f5e39b66ebbb2d13892c3c8add705c4
J-o-h-n-n-y <evgenij302@outlook.com>
2024-07-03 09:52:32 +0300

    graph

--- src/components/Graph.jsx
+++ src/components/Graph.jsx
@@ -1,61 +1,6 @@
 import React, { useEffect, useRef } from 'react';
 import ForceGraph2D from 'react-force-graph-2d';
 
-// Данные графа
-const data = {
-  nodes: [
-    { id: '0', title: 'ВХОД' },
-    { id: '1', title: '1' },
-    { id: '2', title: '2' },
-    { id: '3', title: '3' },
-    { id: '4', title: '4' },
-    { id: '5', title: '5' },
-    { id: '6', title: '6' },
-    { id: '7', title: '7' },
-    { id: '8', title: '8' },
-    { id: '9', title: '9' },
-    { id: '10', title: '10' },
-    { id: '11', title: '11' },
-    { id: '12', title: '12' },
-    { id: '13', title: '13' },
-    { id: '14', title: '14' },
-    { id: '15', title: '15' },
-    { id: '16', title: '16' },
-    { id: '17', title: '17' },
-    { id: '18', title: '18' },
-    { id: '19', title: '19' },
-    { id: '20', title: '20' },
-    { id: '21', title: '21' },
-  ],
-  links: [
-    { source: '0', target: '1' },
-    { source: '1', target: '2' },
-    { source: '2', target: '3' },
-    { source: '2', target: '4' },
-    { source: '2', target: '5' },
-    { source: '4', target: '5' },
-    { source: '5', target: '6' },
-    { source: '6', target: '7' },
-    { source: '6', target: '8' },
-    { source: '6', target: '9' },
-    { source: '8', target: '9' },
-    { source: '9', target: '10' },
-    { source: '10', target: '11' },
-    { source: '10', target: '15' },
-    { source: '11', target: '12' },
-    { source: '11', target: '13' },
-    { source: '11', target: '14' },
-    { source: '13', target: '14' },
-    { source: '14', target: '16' },
-    { source: '16', target: '17' },
-    { source: '17', target: '18' },
-    { source: '17', target: '19' },
-    { source: '18', target: '20' },
-    { source: '20', target: '21' },
-  ],
-};
-
-
 const Graph = (props) => {
   const fgRef = useRef();
 
@@ -75,10 +20,10 @@ const Graph = (props) => {
   useEffect(() => {
     const fg = fgRef.current;
 
-    data.nodes.forEach((node, i) => {
-      node.fx = 0; 
-      node.fy = i * 100; 
-    });
+    // data.nodes.forEach((node, i) => {
+    //   node.fx = 0; 
+    //   node.fy = i * 100; 
+    // });
 
     fg.d3Force('link').distance(20); 
     fg.d3Force('charge').strength(-200); 
@@ -124,7 +69,7 @@ const Graph = (props) => {
         ctx.fillStyle = 'transparent'; 
         ctx.fillText(node.title, node.x, node.y);
       }}
-      enableNodeDrag={false} 
+      enableNodeDrag={true} 
       enableZoomPanInteraction={true}
     />
     </div>
