b397c0127544e2055e632ab1d6f65e6587fe623c
MOMGrom <gromache.mom@yandex.ru>
2024-07-03 10:22:57 +0300

    matrix2

--- src/components/Matrix.jsx
+++ src/components/Matrix.jsx
@@ -1,6 +1,8 @@
 import style from "./Matrix.module.css";
 
 function Matrix(props) {
+    const rowCount = props.Matrix.length;
+    const colCount = props.Matrix[0].length;
 
     return (
         <div className={style.main}>
@@ -8,20 +10,26 @@ function Matrix(props) {
                 Матрица достижимости
             </div>
             <div className={style.matrixArea}>
-                {props.Matrix.map((string, index) => {
-                    return (
-                        <div key={index} className={style.matrixString}>
-                            {string.map((unit, index) => {
-                                return (
-                                    <div key={index} className={style.matrixUnit}>{unit}</div>
-                                )
-                            })}
-                        </div>
-                    )
-                })}
+                {/* Column Numbers */}
+                <div className={style.colNumber}></div>
+                {Array.from({ length: colCount }).map((_, colIndex) => (
+                    <div key={colIndex} className={style.colNumber}>{colIndex + 1}</div>
+                ))}
+
+                {props.Matrix.map((row, rowIndex) => (
+                    <div key={rowIndex} className={style.matrixString}>
+                        {/* Row Number */}
+                        <div className={style.rowNumber}>{rowIndex + 1}</div>
+                        {row.map((unit, colIndex) => (
+                            <div key={colIndex} className={style.matrixUnit} data-value={unit}>
+                                {unit}
+                            </div>
+                        ))}
+                    </div>
+                ))}
             </div>
         </div>
-    )
+    );
 }
 
 export default Matrix;
